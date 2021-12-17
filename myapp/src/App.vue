d32e05c16c49fa0f85234d0e6c2df74d25c57126
OHMphatcha <phatcharaphol@hotmail.com>
2021-12-17 19:26:42 +0700

    Day2

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -2,49 +2,74 @@
   <div>
     <h1>learn : v-model</h1>
     <ul>
-      <li><router-link to="/">home</router-link></li>
-      <li><router-link to=learn1>learn1</router-link></li>
-      <li><router-link to=AddReduce>AddReduce</router-link></li>
-      <li><router-link to=message>message</router-link></li>
-      <li><router-link to=EventMethod>EventMethod</router-link></li>
-      <li><router-link to=DataVariable>Data and Variable</router-link></li>
-      <li><router-link to=DictionaryArray>Dictionary and array</router-link></li>
-      <li><router-link to=styleGlobalLocal>style</router-link></li>
-      <li><router-link to=compute>compute</router-link></li>
+      <li>
+        <button><router-link to="/">home</router-link></button>
+      </li>
+      <li>
+        <button><router-link to="learn1">learn1</router-link></button>
+      </li>
+      <li>
+        <button><router-link to="AddReduce">AddReduce</router-link></button>
+      </li>
+      <li>
+        <button><router-link to="message">message</router-link></button>
+      </li>
+      <li>
+        <button><router-link to="EventMethod">EventMethod</router-link></button>
+      </li>
+      <li>
+        <button>
+          <router-link to="DataVariable">Data and Variable</router-link>
+        </button>
+      </li>
+      <li>
+        <button>
+          <router-link to="DictionaryArray">Dictionary and array</router-link>
+        </button>
+      </li>
+      <li>
+        <button><router-link to="styleGlobalLocal">style</router-link></button>
+      </li>
+      <li>
+        <button><router-link to="compute">compute</router-link></button>
+      </li>
+      <li>
+        <button><router-link to="Binding">Binding</router-link></button>
+      </li>
+      <li>
+        <button><router-link to="inputVmodel">inputVmodel</router-link></button>
+      </li>
     </ul>
-  
-    
+
     <router-view class="box"></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name:"app",
+  name: "app",
   data() {
     return {
-      message:"",
-      
-    }
+      message: "",
+    };
   },
-}
+};
 </script>
 
 <style scoped>
-  ul{
-    margin: 0;
-    padding: 0;
-  }
-  li{
-    display: inline;
-    margin:5px;
-    padding: 10px;
-    margin-top: 20px;
-    border: 2px solid #333;
-  }
-  .box{
-    margin: 20px;
-    padding: 20px;
-    border: 3px solid crimson;
-  }
+ul {
+  margin: 0;
+  padding: 0;
+}
+li {
+  display: inline;
+  margin: 5px;
+  padding: 5px;
+  margin-top: 20px;
+}
+.box {
+  margin: 20px;
+  padding: 20px;
+  border: 3px solid crimson;
+}
 </style>
