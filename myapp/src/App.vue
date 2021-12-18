3c454d20a06e3961c2a2cfabdafe3455b53b668f
OHMphatcha <phatcharaphol@hotmail.com>
2021-12-18 22:02:41 +0700

    Day3-1

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -2,19 +2,33 @@
   <div>
     <h1>learn : v-model</h1>
     <headerr/>
-    
-
-    <router-view class="box"></router-view>
+    <Content/>
+    <Footer @onClock="onClock" title="Copyright 2021 by Ohmaaagoc Ltd." color="black"/>
+    <div class="clock">{{time}}</div>
   </div>
 </template>
 
 <script>
 import headerr from '@/components/layout/headerr.vue';
-
+import Footer from './components/layout/footer.vue';
+import Content from './components/layout/content.vue';
+import moment from "moment";
 export default {
   name: "app",
+  methods: {
+    onClock(value){
+      this.time = moment(value).format('MM/DD/YYYY hh:mm:ss')
+    }
+  },
+  data() {
+    return {
+      time: ""
+    }
+  },
   components:{
-    headerr
+    headerr,
+    Footer,
+    Content
   },
   
 };
@@ -25,4 +39,8 @@ export default {
   margin: 20px;
   padding: 20px;
 }
+.clock{
+  font-size: 12px;
+}
+
 </style>
---
+++ myapp/src/components/layout/content.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="content" class="content-wrapper">
+      <router-view class="box"></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Content"
+}
+</script>
+
+<style>
+.content-wrapper{
+  margin: 24px;
+  padding: 24px;
+  border:1px dashed gray;
+}
+</style>
