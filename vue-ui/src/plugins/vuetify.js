521a465c20075c1dfe638272b88ba29bc474b44b
OHMphatcha <phatcharaphol@hotmail.com>
2021-12-20 22:50:47 +0700

    Day4-3

--- vue-ui/src/plugins/vuetify.js
+++ vue-ui/src/plugins/vuetify.js
@@ -3,4 +3,15 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    theme: {
+      themes: {
+        light: {
+          primary: '#FA5C00',
+          secondary: '#ff00ff',
+          accent: '#8c9eff',
+          error: '#b71c1c',
+        },
+      },
+    },
+  })
--- vue-ui/src/views/Home.vue
+++ vue-ui/src/views/Home.vue
@@ -1,6 +1,27 @@
 <template>
   <div id="home">
-    <h1>Home dsadsad</h1>
+    <v-container>
+      <h1>Home</h1>
+      <p class="green lighten-1 red--text text--darken-1">Ohmaaagoch</p>
+      <p class="green lighten-2 red--text text--darken-2">Ohmaaagoch</p>
+      <p class="green lighten-3 red--text text--darken-3">Ohmaaagoch</p>
+      <p class="green lighten-4 red--text text--darken-4">Ohmaaagoch</p>
+      <p class="green lighten-5 red--text text--darken-5">Ohmaaagoch</p>
+      <p class="blue lighten-1 red--text text--darken-1">Ohmaaagoch</p>
+      <p class="blue lighten-2 red--text text--darken-2">Ohmaaagoch</p>
+      <p class="blue lighten-3 red--text text--darken-3">Ohmaaagoch</p>
+      <p class="blue lighten-4 red--text text--darken-4">Ohmaaagoch</p>
+      <p class="blue lighten-5 red--text text--darken-5">Ohmaaagoch</p>
+
+      <p class="cyan accent-4 white--text text--darken-4 text-lowercase">Ohmaaagoch lowercase</p>
+      <p class="cyan accent-4 white--text text--darken-4 text-uppercase">Ohmaaagoch uppercase</p>
+      <p class="cyan accent-4 white--text text--darken-4 text-captitalize">Ohmaaagoch capitalize</p>
+      
+      <h1 class="cyan accent-4 white--text text--darken-4 text-lowercase">Ohmaaagoch lowercase</h1><br>
+      <h1 class="cyan accent-4 white--text text--darken-4 text-uppercase">Ohmaaagoch uppercase</h1><br>
+      <h1 class="cyan accent-4 white--text text--darken-4 text-captitalize">Ohmaaagoch capitalize</h1>
+      
+    </v-container>
   </div>
 </template>
 
